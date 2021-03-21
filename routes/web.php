7e69982a0270e0f57f78e3abe9a0ca58a57bd13f
LSheNNawY <devshennawy@gmail.com>
2021-03-21 23:34:37 +0200

    Lab 4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\PostsController;
 
 
 Route::get('/', function () {
@@ -11,6 +13,13 @@ Route::get('/', function () {
 Auth::routes();
 Route::resource("posts", PostsController::class)->middleware('auth');
 
-
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+    dd($user);
+});
